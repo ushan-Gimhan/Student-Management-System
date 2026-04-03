d1662ca9257e8d72142f76477e4a766bae62d1cd
ushan-Gimhan <ushangimhan@gmail.com>
2026-04-03 11:59:57 +0530

    feat: implement StudentMapper for DTO-entity conversion

---
+++ src/main/java/com/sms/student_management/mapper/StudentMapper.java
@@ -0,0 +1,25 @@
+package com.sms.student_management.mapper;
+
+import com.sms.student_management.dto.StudentDTO;
+import com.sms.student_management.entity.Student;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StudentMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Student toEntity(StudentDTO dto) {
+        return modelMapper.map(dto, Student.class);
+    }
+
+    public StudentDTO toDTO(Student entity) {
+        return modelMapper.map(entity, StudentDTO.class);
+    }
+}
