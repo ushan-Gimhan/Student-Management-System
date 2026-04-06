ad9e590ea896b19f664af4e8647af2fa17e3a5ce
ushan-Gimhan <ushangimhan@gmail.com>
2026-04-06 08:27:58 +0530

    feat: add validation dependency and annotations to Student entity

--- src/main/java/com/sms/student_management/entity/Student.java
+++ src/main/java/com/sms/student_management/entity/Student.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +17,14 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Course is required")
     private String course;
+
 }
