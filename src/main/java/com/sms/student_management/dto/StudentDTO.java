929d7bd2b035b1f9f4a1a852393adca10bd879da
ushan-Gimhan <ushangimhan@gmail.com>
2026-04-08 11:47:48 +0530

    feat(dto): add input validation to StudentDTO

--- src/main/java/com/sms/student_management/dto/StudentDTO.java
+++ src/main/java/com/sms/student_management/dto/StudentDTO.java
@@ -1,5 +1,7 @@
 package com.sms.student_management.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +11,15 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 public class StudentDTO {
     private Long id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Course is required")
     private String course;
 
     private String profileImageUrl;
